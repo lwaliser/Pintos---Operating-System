6b0a4122bda61f8bddbe42208d13c844ca0ec14f
lwaliser <waliser@ksu.edu>
2016-10-05 11:20:35 -0500

    Update syscall.h

--- syscall.h
+++ syscall.h
@@ -3,7 +3,7 @@
 
 void syscall_init (void);
 
-struct lock thread_lock;
-bool valid_pointer (const void *);
+struct lock thread_lock; //a lock to make sure that only one thread is accessing the file system at a time
+bool valid_pointer (const void *); //checks to make sure you are using a valid pointer
 
 #endif /* userprog/syscall.h */
