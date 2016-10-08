dab9bc7e8400108683318ff27a03cfee7c455b13
Benjamin Stegeman <bstegeman17@gmail.com>
2016-10-07 21:26:26 -0500

    wrote some syscall_handler code, probably doesn't work though

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -16,5 +16,24 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  
+  const struct syscall *sc;
+  unsigned call_nr;
+  int args[3];
+
+  /* Get the system call. */
+  copy_in (&call_nr, f->esp, sizeof call_nr);
+  if (call_nr >= sizeof syscall_table / sizeof *syscall_table) 
+    thread_exit ();
+  sc = syscall_table + call_nr;
+
+  /* Get the system call arguments. */
+  ASSERT (sc->arg_cnt <= sizeof args / sizeof *args);
+  memset (args, 0, sizeof args); 
+  copy_in (args, (uint32_t *) f->esp + 1, sizeof *args * sc->arg_cnt);
+
+  /* Execute the system call,      
+     and set the return value. */ 
+  f->eax = sc->func (args[0], args[1], args[2]);
   thread_exit ();
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,4 +3,29 @@
 
 void syscall_init (void);
 
+  typedef int syscall_function (int, int, int);
+  /* A system call */
+  struct syscall
+    {
+      size_t arg_cnt;          /* Number of arguments. */
+      syscall_function *func;  /* Implementation. */
+    };
+  /* Table of system calls. */
+  static const struct syscall syscall_table[] =
+    {
+      {0, (syscall_function *) halt},
+      {1, (syscall_function *) exit},
+      {1, (syscall_function *) exec},
+      {1, (syscall_function *) wait},
+      {2, (syscall_function *) create},
+      {1, (syscall_function *) remove},
+      {1, (syscall_function *) open},
+      {1, (syscall_function *) filesize},
+      {3, (syscall_function *) read},
+      {3, (syscall_function *) write},
+      {2, (syscall_function *) seek},
+      {1, (syscall_function *) tell},
+      {1, (syscall_function *) close}
+    }
+
 #endif /* userprog/syscall.h */
